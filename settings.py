96dddf0c7e0596a32f23add9dc32bb0a50a0c73b
v1zerty <artemmaydanov2021@gmail.com>
2024-08-13 01:39:47 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,20 @@
+
+token = "7396083475:AAH_lUShoCQzYzZYibu2v73ma7BBHys-R4A"
+
+botname = 'SubbCoinBot'
+
+admins = [6832231878] #админы бота через запятую
+
+LITTLE_SUBCOIN_TO_GET_SUBS = 5 #минимум сабкоинов для раскрутки канала
+
+ref_count = 2 #реферальный бонус (сабкоины)
+
+REF_BONUS = 2
+
+FINE_FOR_UNSUBSCRIBING = -5 #Число должно быть отрицательным и целым
+
+SUBSCRIPTION_TERM = 7 #Какое время юзер должен оьязательно быть подписан на канал?
+
+LINK_TO_INTRODUCTION_AND_RULES = 'https://telegra.ph/Vvedenie-v-bota-a-takzhe-pravila-06-09' #ссылка на правила и инструкцию
+
+LINK_TO_CHAT_OF_BOT = '@SubCoinCHATT' #ссылка на чат
